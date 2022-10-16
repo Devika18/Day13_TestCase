1cd2de8aea0d51b562e1103dce1460fd689b037d
Devika18 <devikankarpe@gmail.com>
2022-10-17 01:51:10 +0530

    added; logic for creating generic class

--- src/GenericMethod/GenericMain.java
+++ src/GenericMethod/GenericMain.java
@@ -2,21 +2,19 @@ package GenericMethod;
 
 import static GenericMethod.GenericMaxInteger.*;
 
-public class GenericMain {
+public class GenericMain<T extends Comparable<T>> {
+
+
     public static void main(String[] args) {
         System.out.println("Welcome to Test Case Program");
-        //Test case 1.1-Given Max Number at 1st Position return the Same Number
-        System.out.printf("Maximum from %d,%d and %d is: %d\n\n", 25, 15, 20, maximum(15, 20, 25));
 
-        //Test case 1.2-Given Max Number at 2nd Position return the Same Number
-        System.out.printf("Maximum from %d,%d and %d is: %d\n\n", 15, 25, 20, maximum(15, 20, 25));
+        Integer x=15, y=25, z=20;
+        Float a=10.0f, b=15.5f, c=10.3f;
+        String str1="Devika",str2="Bhagyashree",str3="Anjali";
 
-        //Test case 1.3-Given Max Number at 3rd Position return the Same Number
-        System.out.printf("Maximum from %d,%d and %d is: %d\n\n", 15, 20, 25, maximum(15, 20, 25));
-
-        //Refactor 1 - Refactor all the 3 to One Generic Method and find the maximum
-        System.out.printf("Maximum from %f,%f and %f is: %f\n\n", 10.0F, 15.5F, 10.3F, getMaximum(10.0F, 15.5F, 10.3F));
-        System.out.printf("Maximum string is: %s\n", MaxString("Devika", "Yash", "Bhagyashree"));
+        new GenericMaxInteger(x,y,z).getMaximum();
+        new GenericMaxInteger(a,b,c).getMaximum();
+        new GenericMaxInteger(str1,str2,str3).getMaximum();
     }
 }
 
--- src/GenericMethod/GenericMaxInteger.java
+++ src/GenericMethod/GenericMaxInteger.java
@@ -1,17 +1,16 @@
 package GenericMethod;
 
-public class GenericMaxInteger {
-    //Generic method
-    public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
-        T max = x;
-
-        if (y.compareTo(max) > 0)
-            max = y;
-        if (z.compareTo(max) > 0)
-            max = z;
-
-        return max;
+public class GenericMaxInteger<T extends  Comparable<T>> {
+    public T firstValue, secondValue, thirdValue;
+    public GenericMaxInteger(T firstInput, T secondInput, T thirdInput) {
+        this.firstValue = firstInput;
+        this.secondValue = secondInput;
+        this.thirdValue = thirdInput;
     }
+    public T getMaximum() {
+        return GenericMaxInteger.getMaximum(firstValue, secondValue, thirdValue);
+    }
+    //Generic method
     public static <T extends Comparable<T>> T getMaximum(T f1, T f2, T f3) {
         //Initializing the Variable
         T max1 = f1;
@@ -20,18 +19,14 @@ public class GenericMaxInteger {
 
         if (f3.compareTo(max1) > 0)
             max1 = f3;
+
+        //Printing the max value
+        printOut(f1, f2, f3, max1);
         return max1;
     }
-
-    public static <T extends Comparable<T>> T MaxString(T u, T v, T w) {
-        //Initializing the Variable
-        T max2 = u;
-        if (v.compareTo(u) > 0)
-            max2 = v;
-
-        if (w.compareTo(max2) > 0)
-            max2 = w;
-        return max2;
+    public static <T> void printOut(T i, T j, T k, T large) {
+        //Printing the max value
+        System.out.printf("Maximum from %s, %s and %s is:  %s\n",i,j,k,large);
     }
 }
 
