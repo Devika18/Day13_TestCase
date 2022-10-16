c9707e43dddf678d266682ffc881a873c3764a32
Devika18 <devikankarpe@gmail.com>
2022-10-17 00:05:21 +0530

    added; logic for finding maximum float value

--- src/GenericMethod/GenericMaxInteger.java
+++ src/GenericMethod/GenericMaxInteger.java
@@ -2,14 +2,15 @@ package GenericMethod;
 
 public class GenericMaxInteger {
     //Generic method
-    public static <T extends Comparable<T>> T maximum(T x,T y,T z){
+    public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
         T max = x;
 
-        if(y.compareTo(max) > 0)
+        if (y.compareTo(max) > 0)
             max = y;
-        if(z.compareTo(max) > 0)
+        if (z.compareTo(max) > 0)
             max = z;
 
         return max;
     }
 }
+
---
+++ src/GenericMethod/MaxFloatUC2.java
@@ -0,0 +1,22 @@
+package GenericMethod;
+
+public class MaxFloatUC2 {
+    public static void main(String[] args) {
+        System.out.println("Program to find Maximum among three float values");
+        Float a = 10.0F, b = 15.5F, c = 10.3F;
+        getMaximum(a,b,c);
+
+    }
+    public static Float getMaximum(Float f1, Float f2, Float f3) {
+        //Initializing the Variable
+        Float max1 = f1;
+        if (f2.compareTo(f1)>0) {
+            max1 = f2;
+        }
+        if (f3.compareTo(max1)>0) {
+            max1 = f3;
+        }
+        System.out.println("Maximum float value among three value is: "+max1);
+        return max1;
+    }
+}
