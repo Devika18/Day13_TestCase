70bfebea2d7ae2bd315c110b03c5307644c2ec1e
Devika18 <devikankarpe@gmail.com>
2022-10-17 09:10:05 +0530

    updated float generic method

--- src/GenericMethod/MaxFloatUC2.java
+++ src/GenericMethod/MaxFloatUC2.java
@@ -3,20 +3,17 @@ package GenericMethod;
 public class MaxFloatUC2 {
     public static void main(String[] args) {
         System.out.println("Program to find Maximum among three float values");
-        Float a = 10.0F, b = 15.5F, c = 10.3F;
-        getMaximum(a,b,c);
-
+        System.out.printf("Maximum of %.2f,%.2f and %.2f is %.2f\n\n", 87.8,17.7,90.5,getMaximum(87.8,17.7,90.5));
     }
-    public static Float getMaximum(Float f1, Float f2, Float f3) {
+    public static <T extends Comparable<T>> T getMaximum(T f1, T f2, T f3) {
         //Initializing the Variable
-        Float max1 = f1;
+        T max1 = f1;
         if (f2.compareTo(f1) > 0)
             max1 = f2;
 
         if (f3.compareTo(max1) > 0)
             max1 = f3;
 
-        System.out.println("Maximum float value among three value is: "+max1);
         return max1;
     }
 }
