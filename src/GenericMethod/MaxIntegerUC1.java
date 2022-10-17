99aadc79d5a92018d5bd38ed2be6a85a5c19c40c
Devika18 <devikankarpe@gmail.com>
2022-10-17 08:53:51 +0530

    added: logic for sub test cases

--- src/GenericMethod/MaxIntegerUC1.java
+++ src/GenericMethod/MaxIntegerUC1.java
@@ -3,21 +3,26 @@ package GenericMethod;
 public class MaxIntegerUC1 {
     public static void main(String[] args) {
         System.out.println("Program to find Maximum among three float values");
-        Integer num1 = 45, num2 = 89, num3 = 58;
-        maximum(num1,num2,num3);
 
+        //TC1.1 - given max integer to the first position
+        System.out.printf("Maximum from %d, %d, and %d is:  %d\n",89,45,58,maximum(45,89,58));
+
+        //TC1.2 - given max integer to the second position
+        System.out.printf("Maximum from %d, %d, and %d is:  %d\n",45,89,58,maximum(45,89,58));
+
+        //TC1.3 - given max integer to the third position
+        System.out.printf("Maximum from %d, %d, and %d is:  %d\n",45,58,89,maximum(45,89,58));
     }
-    public static Integer maximum(Integer n1, Integer n2, Integer n3) {
+    public static <T extends Comparable<T>> T maximum(T n1, T n2, T n3) {
 
         //Initializing the Variable
-        Integer max = n1;
+        T max = n1;
         if (n2.compareTo(n1) > 0)
             max = n2;
 
         if (n3.compareTo(max) > 0)
             max = n3;
 
-        System.out.println("Maximum Integer value among three value is: "+max);
         return max;
     }
 }
